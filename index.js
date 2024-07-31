e16b0a09e5b37b1118a493c36feaf0571413bf38
Tisaac1 <lashay_isaac@yahoo.com>
2024-07-31 16:20:48 -0400

    update new

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 import {images} from './fetchImages.js';
+import {email} from './validateemail.js'
 
 
-const base_URL = 'https://random-d.uk/api'
+const base_URL = 'https://random-d.uk/api/randomimg4'
 const gallery = document.getElementById('gallery');
 const loadmoreBtn = document.getElementById('loadMoreBtn');
 let page = 1;
 
 const fetchDucks = async (breed) => {
     try {
-        const response = await axios (`${base_URL}`/"https://random-d.uk/api/randomimg");
-        if(!response.ok) {
+        const response = await images.axios (`${base_URL}`) 
             throw new Error('Failed to fetch duck gallary.');
-        }
-
-        const data = await response.json();
-        images.push(...data.images);
-        totalPages = data.totalPages;
+        
+        const email = await response.json();
+        images.push(...images.email);
+        totalPages = email.totalPages;
         page++;
             } catch (error) {
              console.log(error);
@@ -27,19 +26,21 @@ const fetchDucks = async (breed) => {
                 const breedName = searchInput.value.trim()
 
                 if (breedName) {
-                    displayBreedInfo(breedName, 'breed-info-4');
+                    displayBreedInfo(breedName, 'breed-info-0');
 
                 } else {
                     alert('Search for the duck breed!')
                 }
+                Image.innerHTML = `<img src="${base_URL}" alt="Random Image" style="max-width: 100%;">`;
+            }
 
                 function handleSearch() {
                     const searchInput = document.getElementById('search-input');
                     searchQuery = searchInput.value.trim();
                     loadDucks(1, searchQuery);
                 }
-            }
+            
 
           
 
-            git push
+          
